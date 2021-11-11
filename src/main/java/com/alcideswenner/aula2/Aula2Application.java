2317cbc873fcaec44c3c3229ebac8b0ab3ee37f7
Alcides Wenner <alcideswenner.jus@gmail.com>
2021-11-11 19:17:23 -0300

    Projeto criado

---
+++ src/main/java/com/alcideswenner/aula2/Aula2Application.java
@@ -0,0 +1,13 @@
+package com.alcideswenner.aula2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula2Application.class, args);
+	}
+
+}
